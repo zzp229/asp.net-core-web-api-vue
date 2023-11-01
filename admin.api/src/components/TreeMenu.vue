559436b8a0bdb8620910e2bb29fa9c2f6113318c
“nongnong” <a13556632276@gmail.com>
2023-11-01 13:41:39 +0800

    需要修改左侧菜单可隐藏

---
+++ admin.api/src/components/TreeMenu.vue
@@ -0,0 +1,24 @@
+<template >
+    <template v-for="item in list" :key="item.index">
+        <el-menu-item :index=item.Index v-if="!item.Children">
+            <IconCom icon="notebook"></IconCom>
+            <template #title><span>{{ item.Name }}</span></template>
+        </el-menu-item>
+        <el-sub-menu :index=item.Index v-else>
+            <template #title>
+                <IconCom icon="folder"></IconCom>
+                <span>{{ item.Name }}</span>
+            </template>
+            <!-- 让孩子重新调用 -->
+            <TreeMenu :list="item.Children"></TreeMenu>
+        </el-sub-menu>
+    </template>
+</template>
+<script setup lang="ts">
+import IconCom from './IconCom.vue';
+import TreeModel from '../class/TreeModel';
+// 定义传入参数是Treemodel数组
+const props = defineProps({ 
+    list: Array<TreeModel>
+}) 
+</script>
